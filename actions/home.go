7ddab70c670a5406571e2d7d1d2e47b8c84d7287
jwoods1 <jdwoodsy1@gmail.com>
2019-12-11 10:43:41 -0700

    updated

--- actions/home.go
+++ actions/home.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"snow_watch/models"
 	"strconv"
+	"time"
 
 	"github.com/gobuffalo/buffalo"
 	"github.com/gobuffalo/pop"
@@ -21,7 +22,6 @@ func HomeHandler(c buffalo.Context) error {
 		return fmt.Errorf("no transaction found")
 	}
 
-	//dailyReports := &models.DailyReports{}
 	stations := &models.Stations{}
 	// Paginate results. Params "page" and "per_page" control pagination.
 	// Default values are "page=1" and "per_page=20".
@@ -36,24 +36,39 @@ func HomeHandler(c buffalo.Context) error {
 
 func getDailyReport(tx *pop.Connection) {
 	stations := models.Stations{}
-	models.DB.All(&stations)
+	tx.All(&stations)
 	fUrl := "https://wcc.sc.egov.usda.gov/reportGenerator/view_csv/customSingleStationReport/daily/"
 	eUrl := ":ID:SNTL%7Cid=%22%22%7Cname/-7,0/SNWD::value,SNWD::delta"
 	for _, s := range stations {
-		url := fUrl + string(s.StationID) + eUrl
+		sId := strconv.Itoa(s.StationID)
+		url := fUrl + sId + eUrl
 		resp, err := http.Get(url)
 		if err != nil {
 			log.Fatalln(err)
 		}
 		records := readCsv(resp.Body)
-		for _, r := range records[1:] {
-			fmt.Println(r)
+		if len(records) > 0 {
+			for _, r := range records[1:] {
+				dr := &models.DailyReport{}
+				date, err := time.Parse("2006-01-02", r[0])
+				if err != nil {
+					log.Fatalln(err)
+				}
+				dr.Date = date
+				dr.StationID = s.ID
+				depth, _ := strconv.ParseFloat(r[1], 64)
+				dr.SnowDepth = depth
+				change, _ := strconv.ParseFloat(r[2], 64)
+				dr.SnowChange = change
+				tx.Create(dr)
+			}
 		}
 
 	}
 }
 
 func getAllStations(tx *pop.Connection) {
+	tx.TruncateAll()
 	url := "https://wcc.sc.egov.usda.gov/reportGenerator/view_csv/customMultipleStationReport/daily/start_of_period/state=%22ID%22%20AND%20network=%22SNTLT%22,%22SNTL%22%20AND%20element=%22SNWD%22%20AND%20outServiceDate=%222100-01-01%22%7Cname/0,0/name,stationId,elevation,latitude,longitude,state.code?fitToScreen=false"
 
 	resp, err := http.Get(url)
@@ -82,8 +97,9 @@ func getAllStations(tx *pop.Connection) {
 		s.Long = lo
 		s.State = r[5]
 
-		tx.Create(s)
+		tx.Save(s)
 	}
+	getDailyReport(tx)
 }
 
 func readCsv(csv_file io.Reader) [][]string {
